95cac3cf04cf85e6064c8751d54c4c876921b72f
kritanshu02 <kritanshu@webkorps.com>
2025-09-04 00:06:28 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button"
+import Link from 'next/link'
+
 const Page = () => {
   return (
     <>
     <p>Hello my name is xyz</p>
     <Button asChild={true} variant={"secondary"}>
-      <a href="https://www.youtube.com/watch?v=7TqsIx_UnFI">
-      Click Me</a>
+      <Link href="https://www.youtube.com/watch?v=7TqsIx_UnFI">
+      Click Me</Link>
       </Button>
     </>
   )
